ee87d43245b2b0f7be2c0eee2427967fbe5736b9
Alexandr Romanishin <alexandr.romanishin@tbwamobile.com>
2019-09-07 22:42:05 +0300

    CleanUp

--- app/src/main/java/com/sirelon/githubapi/feature/search/ui/SearchRepoViewModel.kt
+++ app/src/main/java/com/sirelon/githubapi/feature/search/ui/SearchRepoViewModel.kt
@@ -1,52 +1,42 @@
 package com.sirelon.githubapi.feature.search.ui
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.liveData
-import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import com.sirelon.githubapi.feature.base.BaseViewModel
 import com.sirelon.githubapi.feature.repository.RepoRepository
 import com.sirelon.githubapi.feature.repository.Repository
 import com.sirelon.githubapi.feature.search.SearchRepository
+import com.sirelon.githubapi.utils.throttle
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 /**
  * Created on 2019-09-05 21:29 for GithubAPi.
  */
+// just debug flag for which approach to use: flow or simple suspend methods with async
+private const val SEARCH_VIA_FLOW = true
+
 class SearchRepoViewModel(
     private val searchRepository: SearchRepository,
     private val itemsRepository: RepoRepository
 ) : BaseViewModel() {
 
-    // just debug flag for which approach to use: flow or simple suspend methods with async
-    val searchViaFlow = true
+    private val searchQueryChannel: SendChannel<String>
 
-    private val searchTrigger = MutableLiveData<String>()
+    val repositoryListLiveData = MutableLiveData<List<Repository>>()
 
-    val repositoryListLiveData: LiveData<List<Repository>> =
-        searchTrigger.switchMap(this::searchByQuery)
+    init {
+        searchQueryChannel = Channel(Channel.CONFLATED)
 
-    // I need to handle errors, so convertion to LiveData I have implemeneted in ViewModel, not in repository
-    private fun searchByQuery(it: String): LiveData<List<Repository>> {
-        if (searchViaFlow) {
-            return searchRepository
-                .searchViaFlows(it)
-                .catch { onError(it) }
-                .asLiveData()
-        } else {
-            return liveData {
-                try {
-                    val list = searchRepository.searchViaAsync(it)
-                    emit(list)
-                } catch (e: Exception) {
-                    onError(e)
-                    MutableLiveData<List<Repository>>()
-                }
-            }
+        viewModelScope.launch {
+            searchQueryChannel
+                .throttle()
+                .consumeEach { searchByQuery(it) }
         }
     }
 
@@ -54,20 +44,31 @@ class SearchRepoViewModel(
         string ?: return
 
         // Post, instead of set will discard all previous data if it not commited
-        searchTrigger.postValue(string)
-
-//        viewModelScope.launch {
-//            val result = withContext(Dispatchers.IO) {
-//                runCatching { searchRepository.searchRepositories(string) }
-//            }
-//
-//            result.exceptionOrNull()?.let(this@SearchRepoViewModel::onError)
-//        }
+        searchQueryChannel.offer(string)
     }
 
     fun markAsViewed(item: Repository) {
         viewModelScope.launch(Dispatchers.IO) {
             itemsRepository.markAsViewed(item)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        searchQueryChannel.close()
+    }
+
+    // I need to handle errors, so choose how need to load data is here, in ViewModel, not in repository
+    private suspend fun searchByQuery(query: String) {
+        if (SEARCH_VIA_FLOW) {
+            return searchRepository
+                .searchViaFlows(query)
+                .catch { onError(it) }
+                .collect { repositoryListLiveData.postValue(it) }
+        } else {
+            val result = runCatching { searchRepository.searchViaAsync(query) }
+            result.exceptionOrNull()?.let(this::onError)
+            result.getOrNull()?.let(repositoryListLiveData::postValue)
         }
     }
 }
--- app/src/main/java/com/sirelon/githubapi/utils/extensions.kt
+++ app/src/main/java/com/sirelon/githubapi/utils/extensions.kt
@@ -11,11 +11,46 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlin.coroutines.CoroutineContext
 
 
 /**
  * Created on 2019-09-05 22:12 for GithubAPi.
  */
+/**
+ * A util function, that make throttle possible on Channel.
+ */
+fun <T> Channel<T>.throttle(
+    wait: Long = 1500,
+    context: CoroutineContext = Dispatchers.Default
+): ReceiveChannel<T> =
+    CoroutineScope(context).produce(context) {
+
+        var mostRecent: T
+
+        consumeEach {
+            mostRecent = it
+
+            // Wait some time
+            delay(wait)
+
+            // receive items from channel until it would not be empty
+            while (!isEmpty) {
+                mostRecent = receive()
+            }
+
+            // Send to SendChannel item, which was the last one
+            send(mostRecent)
+        }
+    }
+
 fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
 }
